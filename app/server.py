1a7a1b7b0e318501dd3ee1ceff03f4015415a529
Restu Prajna <restuprajna@Restus-MacBook-Air.local>
2024-02-13 11:43:43 +0800

    first commit

---
+++ app/server.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langserve import add_routes
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
+
+# Edit this to add the chain you want to add
+from rag_google_cloud_vertexai_search import chain as rag_google_cloud_vertexai_search_chain
+
+add_routes(app, rag_google_cloud_vertexai_search_chain, path="/rag-google-cloud-vertexai-search")
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ packages/rag-google-cloud-vertexai-search/rag_google_cloud_vertexai_search/chain.py
@@ -0,0 +1,106 @@
+import os
+
+from langchain.retrievers import GoogleVertexAISearchRetriever
+from langchain_community.chat_models import ChatVertexAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+# Get project, data store, and model type from env variables
+# project_id = os.environ.get("GOOGLE_CLOUD_PROJECT_ID")
+# data_store_id = os.environ.get("DATA_STORE_ID")
+# model_type = os.environ.get("MODEL_TYPE")
+
+# if not data_store_id:
+#     raise ValueError(
+#         "No value provided in env variable 'DATA_STORE_ID'. "
+#         "A  data store is required to run this application."
+#     )
+
+
+
+
+# Set LLM and embeddings
+# model = ChatVertexAI(model_name=model_type, temperature=0.0)
+
+# Create Vertex AI retriever
+# retriever = GoogleVertexAISearchRetriever(
+#     project_id=project_id, search_engine_id=data_store_id
+# )
+
+# RAG prompt
+# template = """Answer the question based only on the following context:
+# {context}
+# Question: {question}
+# """
+# prompt = ChatPromptTemplate.from_template(template)
+
+# RAG
+# chain = (
+#     RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
+#     | prompt
+#     | model
+#     | StrOutputParser()
+# )
+
+llm = ChatGoogleGenerativeAI(model="gemini-pro")
+
+
+
+user_prompt = "Saya akan menguji kemampuan siswa saya tentang Menilai penguasaan pengetahuan dan kemampuan Pilar Negara yaitu Mampu membentuk karakter positif melalui pemahaman dan pengamalan nilai-nilai dalam pancasila, UUD 1945, NKRI, dan Bhinneka Tunggal Ika dalam pembelajaran Seleksi Kompetensi Dasar - Tes Wawasan Kebangsaan sub bagian Pilar Negara. Sekarang, saya ingin membuat soal untuk menguji kemampuan siswa tersebut. Buatlah soal baru berbentuk pilihan ganda dengan tujuan Pemahaman materi Pancasila sebagai Paradigma Pembangunan, dengan harapan Mengerti dan memahami tentang pilar negara serta Pancasila sebagai dasar, falsafah dan ideologi negara, Mengerti dan memahami konsep Pancasila sebagai ideologi terbuka dan sumber nilai, serta butir-butir pengamalan Pancasila berdasarkan Bloom Taksonomi 2001 level Menganalisis (C4). Gunakan peristiwa nyata yang terjadi baru-baru ini di Indonesia sebagai stimulus, kemudian buat soal berdasarkan kasus tersebut. Pilihan ganda dibuat sekreatif mungkin dengan 5 opsi. Opsi jawaban harus beragam dan logis namun gunakan pengecoh yang mirip untuk menyamarkan kunci jawaban. Hindari bahasa dan kalimat yang terlalu sederhana. Soal harus memenuhi kaidah penulisan soal pilihan ganda yang baik dan benar. Soal dibuat beserta pembahasan mengapa pilihan yang benar itu benar."
+
+ANSWER_PROMPT = ChatPromptTemplate.from_template(
+    """selalu ikuti instruksi berikut:
+kamu adalah ahli kebangsaan, bahasa dan psikolog tugasmu hanya membuat soal, kamu tidak bisa langsung berkomunikasi dengan pengguna karena kamu hanya bisa merespon dengan soal, soal yang dibuat berdasarkan pada level bloom taksonomi yang diminta. 
+
+kamu akan menerima user prompt yang sama berkali-kali untuk membuat soal, jadi teruslah membuat soal
+
+Level Bloom Taksonomi: mengingat, memahami, menerapkan, menganalisis, mengevaluasi dan mencipta.
+
+saya sudah tambahkan file yang berisi kata kunci kognitif pada setiap level taksonomi bloom gunakan file tersebut sebagai tambahan referensi dalam membuat soal
+
+Jangan batasi kreatifitas soal pada referensi, kamu bebas gunakan pengetahuanmu dalam membuat konteks.
+
+respon hanya berupa soal dalam bentuk json dengan struktur:  
+-question, 
+-answers[option (A-E), answer, order (1-5) , score (0 or 5), is_true(true or false)],
+-explanation
+
+JANGAN merespon apapun selain soal berupa JSON 
+
+
+dari instruksi tersebut lakukan task berikut
+
+    
+    Task: "{task}"
+    Answer:
+    """
+)
+
+
+
+
+chain = (
+    {"task": RunnablePassthrough()}
+    | ANSWER_PROMPT
+    | llm
+    | StrOutputParser()
+)
+
+# ans = chain.invoke(user_prompt)
+
+
+
+
+
+# Add typing for input
+class Question(BaseModel):
+    __root__: str
+    # input:InputPrompt
+    # metadata: str
+
+
+chain = chain.with_types(input_type=Question)
