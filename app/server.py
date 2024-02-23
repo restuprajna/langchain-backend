6ac4e037a63f695023f13db8cbf461f8fdec0f85
restuprajna <prajna.restu.rp@gmail.com>
2024-02-23 15:32:18 +0800

    update batch method to api

--- app/server.py
+++ app/server.py
@@ -6,14 +6,14 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Header, status, Depends, Request, Response
 from fastapi.security import APIKeyHeader, HTTPBearer
 from langserve import APIHandler
-from langsmith import Client
+# from langsmith import Client
 
 import os
 load_dotenv()
 
 app = FastAPI()
 
-client = Client()
+# client = Client()
 
 # add comment teast dev
 
@@ -57,30 +57,54 @@ async def protected_route(token: str = Depends(validate_token)):
 
 # Edit this to add the chain you want to add
 from rag_google_cloud_vertexai_search import chain as rag_google_cloud_vertexai_search_chain
+from openai_api import chain as openai_api_chain
 
 # add_routes(app, rag_google_cloud_vertexai_search_chain, path="/rag-google-cloud-vertexai-search")
 vertex_api_handler = APIHandler(rag_google_cloud_vertexai_search_chain, path="/vertex-ai")
-@app.post("/vertex-ai/invoke/{instance_id}", include_in_schema=True)
+@app.post("/vertex-ai/{instance_id}/invoke", include_in_schema=True)
 async def protected_route_openai(instance_id: str, token: str = Depends(validate_token), request: Request = None):
     """
     Route protected by token validation.
     """
-    path = f"/vertex-ai/invoke/{instance_id}"
+    path = f"/vertex-ai/{instance_id}/invoke"
     response = await invoke_api(rag_google_cloud_vertexai_search_chain, path, request)
     return response
 
-from openai_api import chain as openai_api_chain
-
-# add_routes(app, openai_api_chain, path="/openai-api")
-
-@app.post("/openai-api/invoke/{instance_id}", include_in_schema=True)
+@app.post("/vertex-ai/{instance_id}/batch", include_in_schema=True)
 async def protected_route_openai(instance_id: str, token: str = Depends(validate_token), request: Request = None):
     """
     Route protected by token validation.
     """
-    path = f"/openai-api/invoke/{instance_id}"
+    path = f"/vertex-ai/{instance_id}/batch"
+    response = await batch_api(rag_google_cloud_vertexai_search_chain, path, request)
+    return response
+
+
+
+
+
+# add_routes(app, openai_api_chain, path="/openai-api")
+
+@app.post("/openai-api/{instance_id}/invoke", include_in_schema=True)
+async def protected_route_openai(instance_id: str, token: str = Depends(validate_token), request: Request = None):
+    """
+    Route protected by token validation.
+    """
+    path = f"/openai-api/{instance_id}/invoke"
     response = await invoke_api(openai_api_chain, path, request)
     return response
+
+@app.post("/openai-api/{instance_id}/batch", include_in_schema=True)
+async def protected_route_openai(instance_id: str, token: str = Depends(validate_token), request: Request = None):
+    """
+    Route protected by token validation.
+    """
+    path = f"/openai-api/{instance_id}/batch"
+    response = await batch_api(openai_api_chain, path, request)
+    return response
+
+
+# define invoke, batch and stream method
 
 async def invoke_api(api_chain, path: str, request: Request) -> Response:
     """
@@ -88,6 +112,13 @@ async def invoke_api(api_chain, path: str, request: Request) -> Response:
     """
     api_handler = APIHandler(api_chain, path=path)
     return await api_handler.invoke(request)
+
+async def batch_api(api_chain, path: str, request: Request) -> Response:
+    """
+    Handle a request for the specified API.
+    """
+    api_handler = APIHandler(api_chain, path=path)
+    return await api_handler.batch(request)
 
 
 if __name__ == "__main__":
